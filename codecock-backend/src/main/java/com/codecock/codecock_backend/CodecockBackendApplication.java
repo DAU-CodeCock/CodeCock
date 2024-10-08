487cc1ec41a010c62982f4da941d52548990be39
YGC20 <ygchun123@gmail.com>
2024-10-08 20:37:29 +0900

    first upload

---
+++ codecock-backend/src/main/java/com/codecock/codecock_backend/CodecockBackendApplication.java
@@ -0,0 +1,14 @@
+package com.codecock.codecock_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class CodecockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecockBackendApplication.class, args);
+	}
+
+}
